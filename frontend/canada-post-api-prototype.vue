36b9220e05b266540b14c7404e3a1ac36b120616
Mazin Badr <mazenalobidy@gmail.com>
2024-05-08 13:19:59 -0400

    Add Flask backend and update Vue.js frontend
    feat: This commit introduces a Flask application that serves as a backend for the Vue.js frontend. The Flask application provides an API endpoint that the Vue.js application can call to retrieve shipping rates and estimated delivery times from the Canada Post API. The Vue.js frontend has been updated to interact with this new API endpoint.

---
+++ frontend/canada-post-api-prototype.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="rate in rates" :key="rate.service_code">
+      <p>Service: {{ rate.service_name }} ({{ rate.service_code }})</p>
+      <p>Price: {{ rate.price }}</p>
+      <p>Estimated Delivery Date: {{ rate.estimated_delivery_date }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      rates: [],
+    };
+  },
+  async created() {
+    const response = await axios.post('/api/rates');
+    this.rates = response.data;
+  },
+};
+</script>
